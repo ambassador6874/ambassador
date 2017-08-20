822f1a6982ded9c00705061c9b89acf6ade25f64
ambassador6874 <navidshetabifard@yahoo.com>
2017-08-21 04:08:37 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {86616552,0,tonumber(our_id)},--Sudo users
+    sudo_users = {86616552,0,tonumber(199577519)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
